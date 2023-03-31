e81129f27624747d2602c4321d6c6afaf9e18891
rafaeltpires <me@rafaeltpires.dev>
2023-03-31 02:14:22 +0100

    Dependency Injection

---
+++ Section 02/src/main/java/com/rafaeltpires/springcoredemo/Coach.java
@@ -0,0 +1,5 @@
+package com.rafaeltpires.springcoredemo;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ Section 02/src/main/java/com/rafaeltpires/springcoredemo/DemoController.java
@@ -0,0 +1,24 @@
+package com.rafaeltpires.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependecy
+    private Coach myCoach;
+
+    // define a constructor for dependency injection
+    @Autowired
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+}
